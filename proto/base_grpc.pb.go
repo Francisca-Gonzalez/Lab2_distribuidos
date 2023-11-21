7e7222d8c59409875aa5902f4220935138018e9a
Francisca-Gonzalez <75760416+Francisca-Gonzalez@users.noreply.github.com>
2023-11-20 22:35:12 -0300

    Update base_grpc.pb.go

--- proto/base_grpc.pb.go
+++ proto/base_grpc.pb.go
@@ -24,7 +24,7 @@ const _ = grpc.SupportPackageIsVersion7
 type MensajeServiceClient interface {
 	Create(ctx context.Context, in *Crearmensaje, opts ...grpc.CallOption) (*Respuestamensaje, error)
 	CreateLista(ctx context.Context, in *ConsultarLista, opts ...grpc.CallOption) (*RespuestaLista, error)
-	CreateMutuo(ctx context.Context, in *EnviarLista, opts ...grpc.CallOption) (*RecibirLista, error)
+	CreateActualiza(ctx context.Context, in *CrearActualizacion, opts ...grpc.CallOption) (*RespuestaActualizacion, error)
 }
 
 type mensajeServiceClient struct {
@@ -53,9 +53,9 @@ func (c *mensajeServiceClient) CreateLista(ctx context.Context, in *ConsultarLis
 	return out, nil
 }
 
-func (c *mensajeServiceClient) CreateMutuo(ctx context.Context, in *EnviarLista, opts ...grpc.CallOption) (*RecibirLista, error) {
-	out := new(RecibirLista)
-	err := c.cc.Invoke(ctx, "/grpc.MensajeService/CreateMutuo", in, out, opts...)
+func (c *mensajeServiceClient) CreateActualiza(ctx context.Context, in *CrearActualizacion, opts ...grpc.CallOption) (*RespuestaActualizacion, error) {
+	out := new(RespuestaActualizacion)
+	err := c.cc.Invoke(ctx, "/grpc.MensajeService/CreateActualiza", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -68,7 +68,7 @@ func (c *mensajeServiceClient) CreateMutuo(ctx context.Context, in *EnviarLista,
 type MensajeServiceServer interface {
 	Create(context.Context, *Crearmensaje) (*Respuestamensaje, error)
 	CreateLista(context.Context, *ConsultarLista) (*RespuestaLista, error)
-	CreateMutuo(context.Context, *EnviarLista) (*RecibirLista, error)
+	CreateActualiza(context.Context, *CrearActualizacion) (*RespuestaActualizacion, error)
 	mustEmbedUnimplementedMensajeServiceServer()
 }
 
@@ -82,8 +82,8 @@ func (UnimplementedMensajeServiceServer) Create(context.Context, *Crearmensaje) 
 func (UnimplementedMensajeServiceServer) CreateLista(context.Context, *ConsultarLista) (*RespuestaLista, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateLista not implemented")
 }
-func (UnimplementedMensajeServiceServer) CreateMutuo(context.Context, *EnviarLista) (*RecibirLista, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateMutuo not implemented")
+func (UnimplementedMensajeServiceServer) CreateActualiza(context.Context, *CrearActualizacion) (*RespuestaActualizacion, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateActualiza not implemented")
 }
 func (UnimplementedMensajeServiceServer) mustEmbedUnimplementedMensajeServiceServer() {}
 
@@ -134,20 +134,20 @@ func _MensajeService_CreateLista_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
-func _MensajeService_CreateMutuo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(EnviarLista)
+func _MensajeService_CreateActualiza_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CrearActualizacion)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(MensajeServiceServer).CreateMutuo(ctx, in)
+		return srv.(MensajeServiceServer).CreateActualiza(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/grpc.MensajeService/CreateMutuo",
+		FullMethod: "/grpc.MensajeService/CreateActualiza",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MensajeServiceServer).CreateMutuo(ctx, req.(*EnviarLista))
+		return srv.(MensajeServiceServer).CreateActualiza(ctx, req.(*CrearActualizacion))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -168,8 +168,8 @@ var MensajeService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _MensajeService_CreateLista_Handler,
 		},
 		{
-			MethodName: "CreateMutuo",
-			Handler:    _MensajeService_CreateMutuo_Handler,
+			MethodName: "CreateActualiza",
+			Handler:    _MensajeService_CreateActualiza_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
